53ef48fe80fa9cbe41139b98a9eeca8b4f7360d3
Ali Ilman Bin Johari <aliilman@protonmail.com>
2023-07-24 10:42:43 +0800

    Set to with issue by default

--- next.config.js
+++ next.config.js
@@ -6,8 +6,8 @@ const nextConfig = {
   // Uncomment the following lines to encounter
   // Failed to load resource: the server responded with a status of 404 (Not Found)
   // error as shown in the README.md
-  // basePath: '/blog',
-  // assetPrefix: '/blog',
+  basePath: '/blog',
+  assetPrefix: '/blog',
 }
 
 module.exports = nextConfig
