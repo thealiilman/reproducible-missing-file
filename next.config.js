72640b2ee88f7cbbb01311f0ae2ec0ba6d8acb8a
Ali Ilman Bin Johari <aliilman@protonmail.com>
2023-07-24 10:39:23 +0800

    Set to no issue by default

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  swcMinify: true,
+  // Uncomment the following lines to encounter
+  // Failed to load resource: the server responded with a status of 404 (Not Found)
+  // error as shown in the README.md
+  // basePath: '/blog',
+  // assetPrefix: '/blog',
 }
 
 module.exports = nextConfig
